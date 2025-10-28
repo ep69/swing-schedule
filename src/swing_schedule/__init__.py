2370f9a72565283674b39d3f3a47ebbaa84206a4
Stanislav Zidek <szidek@redhat.com>
2025-10-28 17:10:59 +0100

    ruff format and check

--- src/swing_schedule/__init__.py
+++ src/swing_schedule/__init__.py
@@ -1,1 +1,9 @@
-from swing_schedule.swing_schedule import *
+from swing_schedule.swing_schedule import (
+    Input as Input,
+    Model as Model,
+    parse as parse,
+    stop as stop,
+    info as info,
+    debug as debug,
+    main as main,
+)
